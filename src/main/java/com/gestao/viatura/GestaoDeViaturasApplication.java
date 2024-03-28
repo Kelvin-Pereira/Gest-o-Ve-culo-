551b6a830934e17cff0de3353540ba1df81e19a8
Kelvin Pereira <kelvinpereira.b18@gmail.com>
2024-03-28 14:16:11 -0300

    Inicio projeto

---
+++ src/main/java/com/gestao/viatura/GestaoDeViaturasApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.viatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeViaturasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoDeViaturasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gestao/viatura/GestaoDeViaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.viatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeViaturasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
